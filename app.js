bef3d24e9ddead1aea877e288df76c8b7a21831e
Ashutosh Mishra <rudrarmishra999@gmail.com>
2021-09-17 02:54:07 +0530

    wip: input and output

---
+++ app.js
@@ -0,0 +1,11 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output")
+
+console.log(txtInput)
+
+btnTranslate.addEventListener("click", function clickEventHandler(){
+    console.log("clicked!!")
+    console.log("input", txtInput.value);
+    console
+})
