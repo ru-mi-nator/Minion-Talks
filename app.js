b071506c416474097db755566cde7617da780b0c
Ashutosh Mishra <rudrarmishra999@gmail.com>
2021-09-17 16:10:27 +0530

    wip

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output")
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("Something went wrong. Try again later.")
+}
 
 console.log(txtInput)
 
-btnTranslate.addEventListener("click", function clickEventHandler(){
-    console.log("clicked!!")
-    console.log("input", txtInput.value);
-    console
-})
+btnTranslate.addEventListener("click", clickEventHandler())
+    
+function clickHandler() {
+
+    // console.log("clicked!!")
+    // console.log("input", txtInput.value);
+    // outputDiv.innerText = "fygdsufyds fstfu " + txtInput.value;
+
+    var inputText = txtInput.value;
+
+    fetch(getTranslationURL(inputText))
+    .then(respone => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedtext;
+    }
+.catch(errorHandler)
+
+}
